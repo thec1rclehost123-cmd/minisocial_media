64d2c03d2fc3fb9e3ad4db1979b136b97c5dcfc4
sahil <sahilbadhe680@gmail.com>
2026-03-03 10:46:56 +0530

    final status

--- src/test/setup.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-// Manually cleanup after each test to ensure fresh DOM state
-afterEach(() => {
-    cleanup();
-});
